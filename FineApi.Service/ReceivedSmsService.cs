9fd5e673e883f48f9769bcf583795316eb35d00f
Roma <rkekua9@gmail.com>
2023-08-25 15:44:53 +0400

    Modify Georgian Alphabet Transilate function

--- FineApi.Service/ReceivedSmsService.cs
+++ FineApi.Service/ReceivedSmsService.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Globalization;
+using System.Text;
 using FineApi.Domain.Abstractions;
 using FineApi.Domain.DTOs;
 using FineApi.Service.Exception;
@@ -13,9 +15,11 @@ public class ReceivedSmsService : IReceivedSmsService
     }
     public async Task UpdateReceivedSms(List<FineDataDto> data)
     {
+        string latinText;
         foreach (var fineData in data)
         {
-            var receivedSms = await _unitOfWorkRepository.ReceivedSmsRepository.SingleAsync(x => x.ReceiptNumber == fineData.ReceiptNumber);
+            TransilateToGeorgian(fineData.ReceiptNumber,out latinText);
+            var receivedSms = await _unitOfWorkRepository.ReceivedSmsRepository.SingleAsync(x => x.ReceiptNumber == latinText);
             if (receivedSms == null) throw new NoReceivedSmsOnThisReceiptNumberException();
             
             receivedSms.FineStatus = fineData.Paid ? Domain.Enums.FineStatus.Paid : Domain.Enums.FineStatus.Unpaid;
@@ -23,6 +27,28 @@ public class ReceivedSmsService : IReceivedSmsService
             await _unitOfWorkRepository.ReceivedSmsRepository.UpdateAsync(receivedSms);
         }
         await _unitOfWorkRepository.SaveAsync();
+    }
+
+    void TransilateToGeorgian(string georgianText,out string eglishText)
+    {
+        eglishText = "";
+        Dictionary<string, string> georgianToLatinMap =new Dictionary<string, string>
+        {
+            { "ა", "a" }, { "ბ", "b" }, { "გ", "g" }, { "დ", "d" }, { "ე", "e" }, { "ვ", "v" }, { "ზ", "z" },
+            { "თ", "t" }, { "ი", "i" }, { "კ", "k" }, { "ლ", "l" },
+            { "მ", "a" }, { "ნ", "n" }, { "ო", "o" }, { "პ", "p" }, { "ჟ", "zh" }, { "რ", "r" }, { "ს", "s" },
+            { "ტ", "t" }, { "უ", "u" }, { "ფ", "f" }, { "ქ", "q" }, { "ღ", "gh" },
+            { "ყ", "y" }, { "შ", "sh" }, { "ჩ", "ch" }, { "ც", "c" }, { "ძ", "dz" }, { "წ", "w" }, { "ჭ", "tch" },
+            { "ხ", "kh" }, { "ჯ", "j" }
+        };
+        foreach (char georgianLetter in georgianText)
+        {
+            if (georgianToLatinMap.TryGetValue(georgianLetter.ToString(), out string latinEquivalent))
+            {
+                eglishText += latinEquivalent;
+            }
+        }
+        eglishText += new string(georgianText.Skip(2).ToArray());
     }
 }
 
