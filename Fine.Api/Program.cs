64920f976bbe230c88ca44d6eb48f8bd2e4d4735
R.Kekua <rkekua@seu.edu.ge>
2023-08-26 17:27:24 +0400

    Modify

--- Fine.Api/Program.cs
+++ Fine.Api/Program.cs
@@ -2,7 +2,29 @@ using Fine.Api;
 using FineApi.Dal;
 using FineApi.Service;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        // angular			react			vue 
+        corsBuilder.WithOrigins("https://localhost:4200", "https://localhost:3000", "https://localhost:8000")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("https://MyProductionSite.Com")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    });
+});
+
 
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistenceService(builder.Configuration);
@@ -23,6 +45,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("DevCors");
 }
 
 app.UseHttpsRedirection();
