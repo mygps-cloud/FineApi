e8cbf2d713d809ca9123f8b797d739ff90ba1e46
Giorgi.Shushanashvili <shushanashvili20@gmail.com>
2023-08-29 13:12:57 +0400

    excpetionaddedToBool

--- FineApi.Service/UserCarInformationService.cs
+++ FineApi.Service/UserCarInformationService.cs
@@ -34,6 +34,8 @@ public class UserCarInformationService : IUserCarInformationService
         {
             if(incrimentDta<1)
                 throw new InvalidOperationException("Can't Process Data,Choose First Car");
+            if(incrimentDta>userCars.Count)
+                throw new InvalidOperationException("Can't Process Data");
             return userCars[incrimentDta-1];
         }
         incrimentDta++;
