27debf6ef7d9165ab2664bfeb4afe03f70ba9194
R.Kekua <rkekua@seu.edu.ge>
2023-08-30 20:12:25 +0400

    Create new table for policeFine

--- FineApi.Domain/Models/SMSFromPoliceVideoFine.cs
+++ FineApi.Domain/Models/SMSFromPoliceVideoFine.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FineApi.Domain.Models;
 
@@ -6,5 +7,10 @@ public class SMSFromPoliceVideoFine
 {
     [Key]
     public int Id { get; set; }
-    
+    public string ReceiptNumber { get; set; }
+    public string Date { get; set; }
+    public string Article { get; set; }
+    public decimal Amount { get; set; }
+    [Column(TypeName = "bit")]
+    public byte Paid { get; set; }
 }
